9c400bda044a8f9fee54bc4d48e88e75aba08c31
Ashia2 <65293834+Ashia2@users.noreply.github.com>
2020-06-29 15:41:01 +0200

    TD-3 Layout - index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+    require_once './layouts/header.php';
+?>
+
+<div id="content">
+    <h1>Witaj!</h1>
+    <h2>Zaplanuj z nami swój dzień.</h2>
+    <a href="#" class="button">Dowiedz się więcej</a>
+</div>
+
+<div class="footer">
+    Work In Progress &copy; <?php echo date("Y");?>
+</div>
