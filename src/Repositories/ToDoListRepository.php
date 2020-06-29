62a680ede2fced00a2a82c13a66d188ae88263a4
Ernest Dobrzyń <ernestdobrzyn@gmail.com>
2020-06-29 18:47:52 +0200

    ToDoListRepository class CRUD

--- src/Repositories/ToDoListRepository.php
+++ src/Repositories/ToDoListRepository.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 namespace App\Repositories;
 use App\Database;
+use PDOException;
 
 class ToDoListRepository
 {
@@ -13,12 +14,84 @@ class ToDoListRepository
         $this->db=new Database;
     }
 
-    public function getAll()
+    public function getAll(int $userID, string $orderBy='id')
     {
         try
         {
-            $query = 'SELECT * FROM '
-            $this->db->pdo->prepare()
+            $query = 'SELECT * FROM `list`
+                       WHERE `user_id`=:userID
+                       ORDER BY '.$orderBy;
+            $stmt=$this->db->pdo->prepare($query);
+            $stmt->bindValue(':userID', $userID, PDO::PARAM_INT);
+            $result = $stmt->execute();
+            if($result===true)
+            {
+                $this->list=$stmt->fetchAll(PDO::FETCH_ASSOC);
+                return $this->list;
+            } else {
+                return;
+            }
+            $stmt->closeCursor();
+        }
+        catch (PDOException $e) {
+            echo "Nie udało się odczytać danych z bazy.";
+        }
+    }
+
+    public function addNewTask(string $task, int $userID)
+    {
+        try
+        {
+            $query = "INSERT INTO `list` (user_id, task)
+                        VALUES (:user_id, :task)";
+            $stmt=$this->db->pdo->prepare($query);
+            $stmt->bindValue(':task', $task, PDO::PARAM_STR);
+            $stmt->bindValue(':userID', $userID, PDO::PARAM_INT);
+            $result = $stmt->execute();
+            return (bool)($result);
+        }
+        catch (PDOException $e)
+        {
+            echo "Nie udało się dodać rekordu do bazy danych";
+            return;
+        }
+    }
+
+    public function updateTask(string $task, int $id)
+    {
+        try
+        {
+            $query = "UPDATE `list`
+                        SET `task`=:task
+                        WHERE `id=:id`";
+            $stmt=$this->db->pdo->prepare($query);
+            $stmt->bindValue(':task', $task, PDO::PARAM_STR);
+            $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+            $result=$stmt->execute();
+            return (bool)($result);
+        }
+        catch (PDOException $e)
+        {
+            echo "Nie udało się dokonać zmiany w rekordzie.";
+            return;
+        }
+    }
+
+    public function deleteTask(int $id)
+    {
+        try
+        {
+            $query = "DELETE FROM `list`
+                        WHERE `id`=:id";
+            $stmt=$this->db->pdo->prepare($query);
+            $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+            $result = $stmt->execute();
+            return (bool)($result);
+        }
+        catch (PDOException $e)
+        {
+            echo "Nie udało się usunąć danego rekordu z bazy danych";
+            return;
         }
     }
 }
