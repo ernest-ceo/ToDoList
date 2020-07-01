7c5da214330747095633d49f01c0764c797b604c
Ernest Dobrzyń <ernestdobrzyn@gmail.com>
2020-07-01 18:13:25 +0200

    wyświetlanie listy

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'dbname' => 'todo',
+    'host' => '127.0.0.1',
+    'user' => 'root',
+    'password' => ''
+];
--- config/menu.php
+++ config/menu.php
@@ -1,6 +1,18 @@
 <?php
-$menu = [
-    "index.php" => "Home",
-    "logowanie.php" => "Logowanie",
-    "rejestracja.php" => "Rejestracja"
-];
+require_once 'session.php';
+if(isset($_SESSION['username']))
+{
+    $menu = [
+        "index.php" => "Home",
+        "logowanie.php" => "Logowanie",
+        "rejestracja.php" => "Rejestracja",
+        "list.php" => "Lista"
+    ];
+
+} else {
+    $menu = [
+        "index.php" => "Home",
+        "logowanie.php" => "Logowanie",
+        "rejestracja.php" => "Rejestracja"
+    ];
+}
