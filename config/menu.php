74bcb0306863869c5ad7017826b79ee059008b83
Ernest Dobrzyń <ernestdobrzyn@gmail.com>
2020-07-03 17:50:32 +0200

    Poprawki

--- config/menu.php
+++ config/menu.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-session_start();
+require_once 'config/session.php';
 if(isset($_SESSION['user']))
 {
     $menu = [
--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-session_start();
+require_once 'config/session.php';
 require_once "config/database.php";
 require_once 'config/menu.php';
 
