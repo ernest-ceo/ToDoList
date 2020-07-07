f9a8704f9e011e260449c9ec2179035c60b4803a
Ernest Dobrzyń <ernestdobrzyn@gmail.com>
2020-07-07 14:32:51 +0200

    CRUD działa

---
+++ layouts/add.php
@@ -0,0 +1,11 @@
+<div calss="row">
+    <div class="col-6 offset-3">
+    <form action="add.php" method="post">
+        <div class="form-group">
+            <label for="1">Treść zadania</label>
+            <textarea class="form-control" id="1" rows="3" name="task"></textarea>
+            <button type="submit" class="btn btn-secondary">Zapisz</button>
+        </div>
+    </form>
+    </div>
+</div>
