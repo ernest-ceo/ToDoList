2c5227c1959d07a1e79f78716c5037dbaa0994f8
Adam Zdanowski <adamzdan97@gmail.com>
2020-07-15 15:09:45 +0200

    podstawowa wartosc w polu edycji zadania

--- layouts/edit.php
+++ layouts/edit.php
@@ -3,7 +3,7 @@
         <form action="edit.php" method="post">
             <div class="form-group">
                 <label for="1"><Treść zadania></label>
-                <textarea class="form-control" id="1" rows="3" name="task" placeholder="<?=$taskToDisplay['task']?>"></textarea>
+                <textarea class="form-control" id="1" rows="3" name="task" placeholder="<?=$taskToDisplay['task']?>"><?=$taskToDisplay['task']?></textarea>
                 <button type="submit" name="taskID" value="<?=$taskToDisplay['id']?>" class="btn btn-secondary">Zatwierdź zmianę</button>
             </div>
         </form>
